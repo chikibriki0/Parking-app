d68e67d0107a07e6b05634bad99f3bf17c0f9675
Максон Самсон <difect22222@gmail.com>
2026-03-24 17:30:01 +0300

    Наконец-то реализовано бронирование только одного места на бекенде и фрон, чужие места занимать нельзя

--- parking_service/init-db/init.sql
+++ parking_service/init-db/init.sql
@@ -39,3 +39,18 @@ INSERT INTO parking_spots (zone_id, spot_number) VALUES
 (1,1),(1,2),(1,3),(1,4),(1,5),
 (2,1),(2,2),(2,3),(2,4),(2,5),
 (3,1),(3,2),(3,3),(3,4),(3,5);
+
+
+CREATE TABLE parking_sessions (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    spot_id INT REFERENCES parking_spots(id),
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP,
+    source TEXT,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE UNIQUE INDEX one_active_parking_per_user
+ON parking_sessions(user_id)
+WHERE end_time IS NULL;
